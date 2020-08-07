d5f97f1bc8664349555fa9439b52f04f20be1edc
Jan Štěch <honza.stech@gmail.com>
2020-08-07 20:08:09 +0200

    Addition of Logger class used for logging messages in a textfile

---
+++ Simple File Encryptor/DebugLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace File_encoder
+{
+    class DebugLogger
+    {
+        private const string logFilePath = "C:\\Users\\honza\\OneDrive\\Online Programování\\C#\\Simple File Encryptor\\debug.log";
+        private static StreamWriter logFileStream = new StreamWriter(DebugLogger.logFilePath);
+
+        public static void log(string text)
+        {
+            DebugLogger.logFileStream.WriteLine(text);
+            DebugLogger.logFileStream.Flush();
+        }
+    }
+}
